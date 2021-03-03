b35e2b37120d6c718d75a42b09f16e208e3dc3ca
Flavia Martins <flavia.martins1991@hotmail.com>
2021-03-03 08:58:32 -0300

    Switch case em C sharp

---
+++ Switch_Case/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace Switch_Case
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboSemana = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cboSemana
+            // 
+            this.cboSemana.FormattingEnabled = true;
+            this.cboSemana.Items.AddRange(new object[] {
+            "Domingo",
+            "Segunda",
+            "Terça",
+            "Quarta",
+            "Quinta",
+            "Sexta",
+            "Sabado"});
+            this.cboSemana.Location = new System.Drawing.Point(116, 144);
+            this.cboSemana.Name = "cboSemana";
+            this.cboSemana.Size = new System.Drawing.Size(182, 33);
+            this.cboSemana.TabIndex = 0;
+            this.cboSemana.Text = "Semana";
+            this.cboSemana.SelectedIndexChanged += new System.EventHandler(this.cboSemana_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(22, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(379, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Escolha um dia da semana";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 333);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboSemana);
+            this.Name = "Form1";
+            this.Text = "Switch Case";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboSemana;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Switch_Case/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Switch_Case
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cboSemana_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cboSemana.Text)
+            {
+                /*O conteudo da combox (cboSemana) será analisado e de acordo com a opção escolhida
+                 aparecerá uma determinada mensagem, caso o usuario escolha Domingo, será msg, 
+                caso seja segunda será outra msg e assim por diante
+                o BREAK serva para para sair de determado comando, neste exemplo o case em exeção*/
+                case "Domingo":
+                    MessageBox.Show("Você escolheu Domingo, que é o primeiro dia ", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+
+                case "Segunda":
+                    MessageBox.Show("Você escolheu Sengunda, que é o segundo dia da semana", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Terça":
+                    MessageBox.Show("Você escolheu Terça que é o terceiro dia da semana", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Quarta":
+                    MessageBox.Show("Você escolheu Quarta que é o quarto dia da semana", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Quinta":
+                    MessageBox.Show("Você escolheu Quinta que é o quinto dia da semana", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Sexta":
+                    MessageBox.Show("Você escolheu Sexta que é o sexto dia da semana", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+                case "Sabado":
+                    MessageBox.Show("Você escolheu Sabado que é o sétimo dia da semana", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    break;
+
+
+
+
+            }
+        }
+    }
+}
